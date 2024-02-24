ee23d95e451beb99179dacfa90991328c89a9806
Mafika <rudolphmafika@gmail.com>
2024-02-24 11:16:07 +0000

    added files and folders

---
+++ ShelfSync/tempscript.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#God back to app
+cd ..
+
+#create templates
+mkdir templates
+
+#go inside
+cd templates
+
+#create index
+touch index.html
+ls
+
+# clean up
+echo "Done!"
+echo pwd
+
