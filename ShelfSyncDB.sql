1a8d6d4ec6ff10e3a81f7f4bb7ee9794edfb6e48
Mafika <rudolphmafika@gmail.com>
2024-02-24 11:35:04 +0200

    created database

--- ShelfSyncDB.sql
+++ ShelfSyncDB.sql
@@ -1,19 +1,44 @@
 #create database
-#CREATE DATABASE ShelfSyncDB
+CREATE DATABASE ShelfSyncDB;
 
 #select database
-USE ShelfSyncDB
+USE ShelfSyncDB;
 
-#Books table
-/*CREATE TABLE Books (
-	resource_id int AUTO_INCREMENT PRIMARY KEY,
-	tile VARCHAR(50) NOT NULL,
+#create tables
+CREATE TABLE resource (
+	resource_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+	title VARCHAR(50) NOT NULL,
+	publisher VARCHAR(50),
+	publication_year DATE
+);
+
+CREATE TABLE Books (
+	isbn VARCHAR(50) PRIMARY KEY,
+	resource_id INT NOT NULL,
+	UNIQUE KEY (resource_id),
 	author VARCHAR(50) NOT NULL,
-	isbn VARCHAR(15) NOT NULL,
-	publication_year DATE NOT NULL,
 	edition VARCHAR(50) NOT NULL,
-	genre VARCHAR(50)
-);*/
+	genre VARCHAR(20) NOT NULL,
+	FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
+);
+
+CREATE TABLE journals (
+	journal_id INT AUTO_INCREMENT PRIMARY KEY,
+	resource_id INT NOT NULL,
+	UNIQUE KEY (resource_id),
+	issue INT NOT NULL,
+	volume INT NOT NULL,
+	FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
+);
+
+CREATE TABLE media (
+	file_id INT AUTO_INCREMENT PRIMARY KEY,
+	resource_id INT NOT NULL,
+	UNIQUE KEY (resource_id),
+	type VARCHAR(20) NOT NULL,
+	FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
+);
+
 
 CREATE TABLE employee (
 	employee_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,11 +61,19 @@ CREATE TABLE patrons (
 	password VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE journals (
-	
+CREATE TABLE transactions (
+	transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+	transaction_type VARCHAR(50) NOT NULL,
+	transaction_date DATE NOT NULL,
+	due_date DATE NOT NULL,
+	patron_id INT NOT NULL,
+	resource_id INT NOT NULL,
+	employee_id INT NOT NULL,
+	FOREIGN KEY (patron_id) REFERENCES patrons(patron_id),
+	FOREIGN KEY (resource_id) REFERENCES resource(resource_id),
+	FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
 
-
-
+#show database tables
 SHOW TABLES;
